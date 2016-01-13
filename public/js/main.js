a17c4592efd9474c79b17113842ed249af08f7af
Colin Gorrie <colin@colingorrie.com>
2016-01-13 10:58:43 -0500

    #intro -> .section-splash

--- public/js/main.js
+++ public/js/main.js
@@ -24,7 +24,7 @@
      * Background image
     /* ---------------------------------------------- */
 
-    $("#intro").backstretch(["img/bg3.jpeg"]);
+    $("#splash").backstretch(["img/bg3.jpeg"]);
 
     /* ---------------------------------------------- /*
      * Navbar
